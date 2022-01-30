ec1c2b0f47b67aa17b243d45b26d02e325a4e80c
hrtsk <tktsghrtsk@gmail.com>
2022-01-30 12:30:40 +0900

    Add: lesson_3(finish)

--- lesson_3/search.py
+++ lesson_3/search.py
@@ -1,23 +1,36 @@
 import pandas as pd
 import eel
+import os
+import re
 
 ### デスクトップアプリ作成課題
-def kimetsu_search(word):
+def kimetsu_search(word, input_path, path):
+    name = re.search(r"C:\\fakepath\\(.*)" , input_path)
+    file_name = name.group(1) #type: ignore
+    
     # 検索対象取得
-    df=pd.read_csv("./source.csv")
-    source=list(df["name"])
-
+    file_path = os.path.abspath(file_name)
+    df=pd.read_csv(file_path)
+    source=list(df["name"]) #type: ignore
     # 検索
     if word in source:
-        print("『{}』はあります".format(word))
+        msg_1 = "『{}』はあります。\n".format(word)
+        print(msg_1)
+        eel.view_log_js(msg_1) #type: ignore
     else:
-        print("『{}』はありません".format(word))
+        msg_2 = "『{}』はありません。\n".format(word)
+        print(msg_2)
+        eel.view_log_js(msg_2) #type: ignore
         # 追加
         #add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
         #if add_flg=="1":
         source.append(word)
+        msg_3 = "『{}』をリストに追加しました。\n".format(word)
+        print(msg_3)
+        eel.view_log_js(msg_3) #type: ignore
     
     # CSV書き込み
     df=pd.DataFrame(source,columns=["name"])
-    df.to_csv("./source.csv",encoding="utf_8-sig")
+    df.to_csv(path, encoding="utf_8-sig")
     print(source)
+    
--- lesson_3/view.py
+++ lesson_3/view.py
@@ -1,14 +1,23 @@
 import eel
 import desktop
 import search
+import re
+import os
 
 app_name="html"
 end_point="index.html"
 size=(700,600)
 
-@ eel.expose
-def kimetsu_search(word):
-    search.kimetsu_search(word)
+@eel.expose
+def kimetsu_search(word, input_path, path):
+    search.kimetsu_search(word, input_path, path)
     
+@eel.expose
+def path_search(path):
+    name = re.search(r"C:\\fakepath\\(.*)" , path)
+    file_name = name.group(1) #type: ignore
+    file_path = os.path.abspath(file_name)
+    eel.abs_path(file_path) #type: ignore
+
 desktop.start(app_name,end_point,size)
 #desktop.start(size=size,appName=app_name,endPoint=end_point)
