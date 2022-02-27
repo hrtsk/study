f00bd69abad8009830d0d0d488739c5128026680
hrtsk <tktsghrtsk@gmail.com>
2022-02-27 15:36:07 +0900

    add: lesson_5 finish

--- lesson_5/pos.py
+++ lesson_5/pos.py
@@ -1,7 +1,5 @@
 import pandas as pd
-import sys
 import datetime
-import eel
 import os
 
 RECEIPT_FOLDER="./receipt"
@@ -9,10 +7,11 @@ os.makedirs(RECEIPT_FOLDER, exist_ok=True)
 
 ### 商品クラス
 class Item:
-    def __init__(self,item_code,item_name,price):
+    def __init__(self,item_code,item_name,price,stock):
         self.item_code=item_code
         self.item_name=item_name
         self.price=price
+        self.stock=stock
 
 ### オーダークラス
 class Order:
@@ -20,11 +19,7 @@ class Order:
         self.item_order_list=[]
         self.item_count_list=[]
         self.item_master=item_master
-        self.set_datetime()
-        
-    def set_datetime(self):
-        self.datetime=datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-    
+
     def add_item_order(self,item_code,item_count):
         self.item_order_list.append(item_code)
         self.item_count_list.append(item_count)
@@ -33,24 +28,24 @@ class Order:
         for item in self.item_order_list:
             print(f"商品コード:{item}")
             
-    # オーダー番号から商品情報を取得する（課題１）
     def get_item_data(self,item_code):
         for m in self.item_master:
             if item_code==m.item_code:
-                return m.item_name,m.price  
-
-    @staticmethod   
+                return m.item_name,m.price,m.stock
+    
+    @staticmethod
     def write_receipt(text):
         with open(RECEIPT_FOLDER + "/" + f'{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.csv' , mode="a", encoding="utf-8_sig") as f: # type: ignore
             f.write(text) 
-            
-# マスタ登録(課題３)
-def add_item_master_by_csv(csv_path):
-    item_master=[]
-    try:
-        item_master_df=pd.read_csv(csv_path,dtype={"item_code":object}) # CSVでは先頭の0が削除されるためこれを保持するための設定
-        for item_code,item_name,price in zip(list(item_master_df["item_code"]),list(item_master_df["item_name"]),list(item_master_df["price"])):
-            item_master.append(Item(item_code,item_name,price))
-        return item_master
-    except:
-        return item_master
+                
+    # マスタ登録(課題３)
+    @staticmethod
+    def add_item_master_by_csv(csv_path):
+        item_master=[]
+        try:
+            item_master_df=pd.read_csv(csv_path,dtype={"item_code":object}) # CSVでは先頭の0が削除されるためこれを保持するための設定
+            for item_code,item_name,price,stock in zip(list(item_master_df["item_code"]),list(item_master_df["item_name"]),list(item_master_df["price"]),list(item_master_df["stock"])):
+                item_master.append(Item(item_code,item_name,price,stock))
+            return item_master
+        except:
+            return item_master
--- lesson_5/view.py
+++ lesson_5/view.py
@@ -1,7 +1,7 @@
 import eel
 import desktop
 import pos
-import sys
+import pandas as pd
 import re
 
 app_name="html"
@@ -11,29 +11,50 @@ size=(710,540)
 total_price = 0
 bought_items = ""
 receipt = ""
-ERROR = "最初からやり直してください"
+count_dic = {}
+first_loop = True
+item_master = []
 
 @eel.expose
 def print_cart_by_py(input_file, code, num):
     # 商品登録
-    item_master = pos.add_item_master_by_csv(input_file)
+    global item_master
+    item_master = pos.Order.add_item_master_by_csv(input_file) #type:ignore
+    # 入力CHECK(マスタ登録)
     if item_master == []:
-        eel.erorrMessage(f"マスタ登録失敗\n{ERROR}") #type:ignore
-        sys.exit()
+        eel.errorMessage("マスタ登録失敗") #type:ignore
+        return
     order = pos.Order(item_master)
+    
+    # 入力CHECK(購入数)
     try:
         re.fullmatch(r'[1-9][0-9]*', num).group() #type:ignore
     except:
         eel.errorMessage("購入数は1以上の整数を入力してください") #type: ignore
+        return
+    
     code_list = []
+    global count_dic
+    global first_loop
     for i in item_master:
-        code_list.append(i.item_code)
-        
+        if first_loop:
+            code_list.append(i.item_code)
+            count_dic[i.item_code] = 0
+        else:
+            code_list.append(i.item_code)
+    first_loop =False
+    
     if code in code_list:
         item_data = order.get_item_data(code)
         order.add_item_order(code, num)
-    
-        item = f'商品:{item_data[0]} 価格:{item_data[1]} 個数:{num}' #type: ignore
+        count_dic[code] += int(num)  #type: ignore
+        
+        # 入力CHECK(在庫数)
+        if item_data[2] - count_dic[code] < 0: #type: ignore
+            eel.errorMessage("在庫不足") #type: ignore
+            return
+        
+        item = f'商品:{item_data[0]} 価格:{item_data[1]} 個数:{num} 在庫:{item_data[2] - count_dic[code]}' #type: ignore
         global total_price
         total_price += int(item_data[1]) * int(num) #type: ignore
         global bought_items
@@ -42,18 +63,39 @@ def print_cart_by_py(input_file, code, num):
         receipt = bought_items + f"合計金額:{total_price}円"
         eel.printReceiptByJs(receipt) #type: ignore
     else:
+        # 入力CHECK(商品コード)
         eel.errorMessage("商品コードエラー") #type: ignore
+        return
 
 @eel.expose       
-def calculate_change_by_py(deposit):
+def calculate_change_by_py(input_file,deposit):
     global total_price
     change = int(deposit) - total_price
+    
+    # 入力CHECK(支払い金)
     if change < 0:
         eel.errorMessage("支払金不足") #type: ignore
+        return
+    
     global receipt
     result = receipt + f'\n\nお預かり:{deposit}円\n' + f'お釣り:{change}円'
     pos.Order.write_receipt(result) #type: ignore
     eel.printReceiptByJs(result) #type: ignore
+    
+    # 在庫管理/CSV上書き
+    overwrite_list = []
+    for item in item_master:
+        item.stock -= count_dic[item.item_code]
+        overwrite_list.append([item.item_code,item.item_name,item.price,str(item.stock)])
+    df = pd.DataFrame(overwrite_list)
+    df.columns=["item_code","item_name","price","stock"]
+    df.to_csv(input_file, sep=",", index=False)
+    
+        
+    
+    #df = pd.DataFrame(item_master)
+    
+    #df.to_csv(input_file)
 
 desktop.start(app_name,end_point,size)
 #desktop.start(size=size,appName=app_name,endPoint=end_point)
