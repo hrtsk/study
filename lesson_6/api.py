96141e17bd49e0d3cb7dad557382be64fc4816aa
hrtsk <tktsghrtsk@gmail.com>
2022-02-28 21:17:56 +0900

    1st commit lesson_6

---
+++ lesson_6/api.py
@@ -0,0 +1,25 @@
+import requests
+import urllib
+
+
+def get_api(url, params: dict):
+    result = requests.get(url, params=params)
+    return result.json()
+
+
+def main():
+    keyword = "鬼滅"
+    url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
+    
+    # パラメータを記述
+    params = {
+        "format": "json",
+        "keyword": keyword,
+        "applicationId": "1019079537947262807",
+        "minPrice": 111
+    }
+    
+    print(get_api(url, params=params))
+
+
+main()
