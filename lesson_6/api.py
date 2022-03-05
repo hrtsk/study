464f3bf4664fb3171fb3fd36d40a7744d94b836b
hrtsk <tktsghrtsk@gmail.com>
2022-03-05 12:34:06 +0900

    add: lesson6 finish

--- lesson_6/api.py
+++ lesson_6/api.py
@@ -1,25 +1,72 @@
 import requests
-import urllib
+import pandas as pd
+import datetime
 
-
+def get_datetime():
+  dt_now = datetime.datetime.now()
+  return dt_now.strftime('%Y%m%d')
+  
 def get_api(url, params: dict):
-    result = requests.get(url, params=params)
-    return result.json()
+  result = requests.get(url, params=params)
+  return result.json()
 
+def task2():
+  url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
+  appid = "1081675625476532194"
+  
+  params = {
+    "format": "json",
+    "keyword": "Python",
+    "applicationId": appid
+  }
+  result = get_api(url, params=params)
+  
+  print(result["Items"][0]["Item"]["itemName"])
+  print(result["Items"][0]["Item"]["itemPrice"])
+  return result
 
-def main():
-    keyword = "鬼滅"
-    url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
+def task3():
+  url = "https://app.rakuten.co.jp/services/api/Product/Search/20170426"
+  appid = "1081675625476532194"
+
+  params = {
+    "format": "json",
+    "productId": "6de7af896684b9e71647d70ddcaec07f",
+    "applicationId": appid
+  }
+
+  result = get_api(url, params=params)
+  #print(json.dumps(result, indent=4))
+  #print(result["items"][0]["itemName"])
+  
+  max = result["Products"][0]["Product"]["maxPrice"]
+  min = result["Products"][0]["Product"]["minPrice"]
+  print(max)
+  print(min)
+  return max, min
+  
+def task4():
+  url = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20170628"
+  appid = "1081675625476532194"
+  genreid = 558929
+  
+  params = {
+    "format": "json",
+    "genreId": genreid,
+    "applicationId": appid
+  }
+
+  result = get_api(url, params=params)
+  
+  csv_list = []
+  column = ["rank", "itemName", "itemPrice"]
+  for i in result["Items"]:
+    item = i["Item"]
+    csv_list.append([item["rank"],item["itemName"],item["itemPrice"]])
     
-    # パラメータを記述
-    params = {
-        "format": "json",
-        "keyword": keyword,
-        "applicationId": "1019079537947262807",
-        "minPrice": 111
-    }
-    
-    print(get_api(url, params=params))
+  df = pd.DataFrame(csv_list, columns=column)
+  df.to_csv(f"{genreid}_{get_datetime()}.csv")
+  return result
 
-
-main()
+if __name__ == '__main__':
+  task3()
--- lesson_6/api_test.py
+++ lesson_6/api_test.py
@@ -1,23 +1,53 @@
-﻿from api import get_api
-import pprint 
+﻿from api import *
+
+def test_get_datetime():
+  assert get_datetime()
 
 def test_get_api():
-    url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?format=json&keyword={PS5}&applicationId=1019079537947262807"
-    res = get_api(url=url)
-    
-    # チェック
-    # 正常系　→　うまくいった時の処理
-    assert len(res["Items"]) >= 1
-    assert res["Items"][0]["Item"]["itemCode"]
-    
-    # 異常系　→　失敗時の処理
-    url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?format=json&keyword={PS5ああああああああああ}&applicationId=1019079537947262807"
-    res = get_api(url=url)
-    
-    assert len(res["Items"]) == 0
+  url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
+  appid = "1081675625476532194"
+  
+  # 正常系 → うまくいった時の処理
+  params = {
+    "format": "json",
+    "keyword": "Python",
+    "applicationId": appid
+  }
+  res = get_api(url, params=params)
 
-def test_2():
-    url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?format=json&keyword=PS5&applicationId=1019079537947262807"
-    res = get_api(url=url)
-    
-    assert res.get("Items") == None
+  assert len(res["Items"]) >= 1
+  assert res["Items"][0]["Item"]["itemCode"]
+
+  # 異常系 → 失敗時の処理
+  url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
+  params = {
+    "format": "json",
+    "keyword": "하히후헤호",
+    "applicationId": appid
+  }
+  res = get_api(url, params=params)
+  
+  assert len(res["Items"]) == 0
+  
+def test_task2():
+  result = task2()
+  
+  assert result["Items"]
+  assert result["Items"][0]["Item"]["itemName"]
+  assert result["Items"][0]["Item"]["itemPrice"]
+  
+
+def test_task3():
+  max, min = task3()
+
+  assert max
+  assert min
+  assert max > min
+  
+def test_task4():
+  result = task4()
+  
+  assert result["Items"]
+  assert len(result["Items"]) >= 1
+  assert result["Items"][0]["Item"]["itemName"]
+  assert result["Items"][0]["Item"]["itemPrice"]
